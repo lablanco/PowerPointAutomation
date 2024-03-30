e8f690f4ec1ef0429d86642a01e2e5d5f6ead4b5
lablanco <lablanco@yahoo.com>
2024-03-29 23:33:12 -0300

    1er commit

--- migit.bat
+++ migit.bat
@@ -1,5 +1,6 @@
 git init
 git add .
-git commit -m "2d commit"
+git commit -m "1er commit"
+git remote add origin https://github.com/lablanco/PowerPointAutomation.git
 git branch -m main
 git push -u origin main
